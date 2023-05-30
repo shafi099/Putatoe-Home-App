18c03912b01e389c8a2e1bb5e782fdb9a706443d
Shafi Shaik <shaikshafieluru@gmail.com>
2023-05-30 23:24:26 +0530

    commit

--- putatoe/src/components/HomeApp.jsx
+++ putatoe/src/components/HomeApp.jsx
@@ -5,9 +5,9 @@ import {
 
     FaMobileAlt, FaTv, FaMoneyBillAlt, FaBullhorn, FaRegNewspaper, FaBlog, FaPeopleArrows, FaWpforms, FaGripfire, FaRegLightbulb, FaHandHoldingWater, FaArrowCircleRight
 
-    
+
 } from "react-icons/fa";
-import { FcShipped, FcOnlineSupport} from "react-icons/fc";
+import { FcShipped, FcOnlineSupport } from "react-icons/fc";
 import ReachargeCardPic from '../pics/RechargeCard.jpg'
 import coverblock from '../pics/coverblock.png'
 import bricks from '../pics/bricks.jpg'
@@ -17,6 +17,10 @@ import cementpic2 from '../pics/cementpic2.jpg'
 import landpic1 from '../pics/landpic1.jpeg'
 import ItemCard from './ItemCard';
 import ContactComp from './ContactComp';
+import logo from '../pics/logo.png'
+import { TiHome, TiUser, TiThList, } from "react-icons/ti";
+import { BsChatTextFill } from "react-icons/bs";
+
 
 const HomeApp = () => {
     return (<>
@@ -46,58 +50,58 @@ const HomeApp = () => {
             <span className='optionsRow1'>
                 <span>
                     <span><FaMobileAlt size='30px' color='#498d95' /></span>
-                    <span>Recharge</span>
+                    <span classname='NavText'>Recharge</span>
                 </span>
                 <span>
                     <span><FaRegNewspaper size='30px' color='#498d95' /></span>
-                    <span>News</span>
+                    <span classname='NavText'>News</span>
                 </span>
                 <span>
                     <span><FaGripfire size='30px' color='#498d95' /></span>
-                    <span>Gas Booking</span>
+                    <span classname='NavText'>Gas Booking</span>
                 </span>
             </span>
             <span className='optionsRow2'>
                 <span>
                     <span>
                         <span><FaTv size='30px' color='#498d95' /></span>
-                        <span>DTH Recharge</span>
+                        <span classname='NavText'>DTH Recharge</span>
                     </span>
                     <span><FaBlog size='30px' color='#498d95' /></span>
-                    <span>Blog</span>
+                    <span classname='NavText'>Blog</span>
                 </span>
                 <span>
                     <span><FaPeopleArrows size='30px' color='#498d95' /></span>
-                    <span>Jobs</span>
+                    <span classname='NavText'>Jobs</span>
                 </span>
             </span>
             <span className='optionsRow3'>
                 <span>
                     <span><FaMoneyBillAlt size='30px' color='#498d95' /></span>
-                    <span>Loans</span>
+                    <span classname='NavText'>Loans</span>
                 </span>
                 <span>
                     <span><FaRegLightbulb size='30px' color='#498d95' /></span>
-                    <span>Electricity Bill</span>
+                    <span classname='NavText'>Electricity Bill</span>
                 </span>
                 <span>
                     <span><FaHandHoldingWater size='30px' color='#498d95' /></span>
-                    <span>Water Bill</span>
+                    <span classname='NavText'>Water Bill</span>
                 </span>
             </span>
             <span className='optionsRow4'>
                 <span>
                     <span><FaBullhorn size='30px' color='#498d95' /></span>
-                    <span>Promotions</span>
+                    <span classname='NavText'>Promotions</span>
                 </span>
                 <span>
                     <span><FaWpforms size='30px' color='#498d95' /></span>
-                    <span>Anonymous<br />Feedback</span>
+                    <span classname='NavText'>Feedback</span>
                 </span>
 
                 <span>
                     <span><FaArrowCircleRight size='30px' color='#498d95' /></span>
-                    <span>See More</span>
+                    <span classname='NavText'>See More</span>
                 </span>
             </span>
         </div>
@@ -107,12 +111,12 @@ const HomeApp = () => {
         <div className='constructionBlock'>
             <span className='titleBlock'>Construction</span>
             <span className='coverblockportion'>
-                <img src={coverblock} alt='coverBlock'/>
+                <img src={coverblock} alt='coverBlock' />
                 <span className='name'>COVER BLOCK</span>
                 <span >
                     <span className='offertext'>UP TO</span>
                     <span className='offertextRed'> 10% OFF</span>
-                    </span>
+                </span>
             </span>
         </div>
 
@@ -120,20 +124,20 @@ const HomeApp = () => {
             <span>Popular Service Products</span>
         </div>
         <div className='normalHead'>
-        <span className='titleBlock'>Construction</span>
+            <span className='titleBlock'>Construction</span>
         </div>
         <div className='DisplayOptions'>
-        <ItemCard pic={bricks} name='Royal Bricks' desc='Brick' price='18000' option1='2000 Pieces' options2='1000 Pieces' off='20000'/>
-        <ItemCard pic={landpic1} name='Dehri Bihar' desc='Morang' price='6272' option1='78 Square Feet' options2=' ' off='7500'/>
-        <ItemCard pic={cementpic1} name='ACC  Gold' desc='Cement' price='485' option1='50 KG' options2='25 KG' off='550'/>
-        <ItemCard pic={cementpic2} name='UltraTech PPC' desc='cement' price='450' option1='50 KG' options2='25 KG' off='550'/>
-        <ItemCard pic={wire} name='Wire' desc='Cable' price='90' option1='Copper' options2='' off='110'/>
+            <ItemCard pic={bricks} name='Royal Bricks' desc='Brick' price='18000' option1='2000 Pieces' options2='1000 Pieces' off='20000' />
+            <ItemCard pic={landpic1} name='Dehri Bihar' desc='Morang' price='6272' option1='78 Square Feet' options2=' ' off='7500' />
+            <ItemCard pic={cementpic1} name='ACC  Gold' desc='Cement' price='485' option1='50 KG' options2='25 KG' off='550' />
+            <ItemCard pic={cementpic2} name='UltraTech PPC' desc='cement' price='450' option1='50 KG' options2='25 KG' off='550' />
+            <ItemCard pic={wire} name='Wire' desc='Cable' price='90' option1='Copper' options2='' off='110' />
         </div>
         <div className='HeaderPart'>
             <span>All Popular Service Providers</span>
         </div>
         <div className='normalHead'>
-        <span className='titleBlock'>Construction</span>
+            <span className='titleBlock'>Construction</span>
         </div>
         <div className='DisplayOptions'>
             <ContactComp letter='G' name='Gangotri Building' />
@@ -152,17 +156,25 @@ const HomeApp = () => {
         <div className='Service'>
             <div className='servicePortion'>
                 <span>
-                   <FcShipped size='100px' /> 
+                    <FcShipped size='100px' />
                 </span>
                 <span>Daily Needs</span>
             </div>
             <div className='servicePortion'>
                 <span>
-                   <FcOnlineSupport size='100px' /> 
+                    <FcOnlineSupport size='100px' />
                 </span>
                 <span>Consultancy</span>
             </div>
         </div>
+        <div className='bottomBar'>
+            <span><TiHome size='35px' color='#498d95' /></span>
+            <span><TiUser size='40px' color='#498d95' /></span>
+            <span><img src={logo} alt='logo' className='logo' /></span>
+            <span><TiThList size='30px' color='#498d95' /></span>
+            <span><BsChatTextFill size='30px' color='#498d95' /></span>
+        </div>
+
     </>)
 }
 
